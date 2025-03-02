1a5201b9f2f56d2fec3b94a27da12997ad071375
Thalia Archibald <thalia@archibald.dev>
2025-03-02 11:12:09 -0800

    Fix errno link name

--- library/core/src/fmt/hook.rs
+++ library/core/src/fmt/hook.rs
@@ -1,6 +1,5 @@
 use crate::ffi::{c_int, c_size_t, c_ssize_t, c_void};
 use crate::fmt;
-
 pub(crate) struct Stderr;
 
 impl fmt::Write for Stderr {
@@ -35,14 +34,42 @@ fn stderr_write(buf: &[u8]) -> Result<usize, i32> {
 
     unsafe extern "C" {
         fn write(fd: c_int, buf: *const c_void, count: c_size_t) -> c_ssize_t;
-        fn errno_location() -> *mut c_int;
     }
 
     let ret = unsafe { write(2, buf.as_ptr() as *const c_void, buf.len().min(READ_LIMIT)) };
-    if ret == -1 {
-        let errno = unsafe { (*errno_location()) as i32 };
-        Err(errno)
-    } else {
-        Ok(ret as usize)
+    if ret == -1 { Err(errno()) } else { Ok(ret as usize) }
+}
+
+fn errno() -> i32 {
+    unsafe extern "C" {
+        #[cfg(not(any(target_os = "dragonfly", target_os = "vxworks", target_os = "rtems")))]
+        #[cfg_attr(
+            any(
+                target_os = "linux",
+                target_os = "emscripten",
+                target_os = "fuchsia",
+                target_os = "l4re",
+                target_os = "hurd",
+            ),
+            link_name = "__errno_location"
+        )]
+        #[cfg_attr(
+            any(
+                target_os = "netbsd",
+                target_os = "openbsd",
+                target_os = "android",
+                target_os = "redox",
+                target_os = "nuttx",
+                target_env = "newlib"
+            ),
+            link_name = "__errno"
+        )]
+        #[cfg_attr(any(target_os = "solaris", target_os = "illumos"), link_name = "___errno")]
+        #[cfg_attr(target_os = "nto", link_name = "__get_errno_ptr")]
+        #[cfg_attr(any(target_os = "freebsd", target_vendor = "apple"), link_name = "__error")]
+        #[cfg_attr(target_os = "haiku", link_name = "_errnop")]
+        #[cfg_attr(target_os = "aix", link_name = "_Errno")]
+        fn errno_location() -> *mut c_int;
     }
+    unsafe { (*errno_location()) as i32 }
 }
